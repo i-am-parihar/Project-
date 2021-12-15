4bbaee050f84d4b10ef053076cc0ecb89550893a
i-am-parihar <paritoshparihar4@gmail.com>
2021-12-15 17:24:52 +0530

    new_update

--- products.js
+++ products.js
@@ -1,1 +1,195 @@
-console.log("here")
+var data = [{
+              image:"https://www.uboric.com/wp-content/uploads/2021/01/715Lt83VEHL._UX695_.png" ,
+              name: "WOODLAND MENS'S CASUAL/PARTY SHOES IN TAN...",
+              price: "₹3,695.00" ,
+              simage1: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQnrGV7NWeLUnbmguf4kQyqxSa3goLaT7vUaMDI1lv_vb9WBsiH" ,
+              simage2: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTxcTdQaash9W0dwpNIdXU2xxosm_BtQoDqAaDT49k9hTpHji6j" ,
+              simage3: "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTrUBehCeaCPYgGJuedGpMccj5l8JYNyv5dcgceW6fqWS1WbaKM" ,
+              simage4: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcR1TL103whBYxQURqkbDKzfXrIJlY1OBvQnZZzdUj0ePsXgWaaF" ,
+              category: "casual" ,
+              size: "10" ,
+              color: "tan" ,
+              clouse: "laceup" ,
+                  },
+            {
+                    image:"https://www.uboric.com/wp-content/uploads/2021/01/id2071black-5-id-black-original-imafp65fhcqmbgkg.png" ,
+                    name: "ID MEN'S CASUAL / PARTY SHOES IN BLACK COLOUR",
+                    price: "₹2,585.00" ,
+                    simage1: "https://www.uboric.com/wp-content/uploads/2021/01/id2071black-5-id-black-original-imafp65fhcqmbgkg-100x100.png" ,
+                    simage2: "https://www.uboric.com/wp-content/uploads/2021/01/id2071black-5-id-black-original-imafp64yrphjgaan-100x100.png" ,
+                    simage3: "https://www.uboric.com/wp-content/uploads/2021/01/id2071black-5-id-black-original-imafp65fzgx9gcg5-100x100.png" ,
+                    simage4: "https://www.uboric.com/wp-content/uploads/2021/01/id2071black-5-id-black-original-imafp65fz99wzhcs-100x100.png" ,
+                    category: "casual" ,
+                    size: "10" ,
+                    color: "black" ,
+                    clouse: "slipon" ,
+               },
+               {
+                image:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_1011_result.jpg" ,
+                name: "GARMONT MEN'S CASUAL/PARTY SHOES IN BLUE COLOUR",
+                price: "₹1,499.00" ,
+                simage1: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1011_result-100x100.jpg" ,
+                simage2: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1012_result-100x100.jpg" ,
+                simage3: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1013_result-100x100.jpg" ,
+                simage4: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1014_result-100x100.jpg" ,
+                category: "casual" ,
+                size: "8" ,
+                color: "blue" ,
+                clouse: "laceup" ,
+           },
+           {
+            image:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0695_result.jpg" ,
+            name: "GARMONT MEN'S CASUAL/PARTY SHOES IN BROWN COLOUR",
+            price: "₹1,299.00" ,
+            simage1: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0695_result-100x100.jpg" ,
+            simage2: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0696_result-100x100.jpg" ,
+            simage3: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0697_result-100x100.jpg" ,
+            simage4: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0698_result-100x100.jpg" ,
+            category: "casual" ,
+            size: "9" ,
+            color: "brown" ,
+            clouse: "laceup" ,
+       },
+       {
+        image:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_1007_result.jpg" ,
+        name: "GARMONT MEN'S CASUAL/PARTY SHOES IN BROWN COLOUR",
+        price: "₹1,499.00" ,
+        simage1: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1007_result-100x100.jpg" ,
+        simage2: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1008_result-100x100.jpg" ,
+        simage3: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1009_result-100x100.jpg" ,
+        simage4: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_1010_result-100x100.jpg" ,
+        category: "casual" ,
+        size: "9" ,
+        color: "brown" ,
+        clouse: "laceup" ,
+   },
+   {
+    image:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result.jpg" ,
+    name: "LEE COOPER MEN’S LOAFERS SHOES IN BLUE COLOUR",
+    price: "₹2,699.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result-100x100.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0688_result-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0689_result-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0690_result-100x100.jpg" ,
+    category: "party" ,
+    size: "10" ,
+    color: "blue" ,
+    clouse: "slipon" ,
+},
+{
+    image:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result.jpg" ,
+    name: "LEE COOPER MEN’S LOAFERS SHOES IN BLUE COLOUR",
+    price: "₹2,699.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0687_result-100x100.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0688_result-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0689_result-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0690_result-100x100.jpg" ,
+    category: "party" ,
+    size: "10" ,
+    color: "blue" ,
+    clouse: "slipon" ,
+},
+{
+    image:"https://www.uboric.com/wp-content/uploads/2021/01/DSC_0638_result.jpg" ,
+    name: "HUSH PUPPIES MEN’S / CASUAL PARTY SHOES IN BLACK COLOUR",
+    price: "₹2,999.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0638_result-100x100.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0639_result-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0640_result-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2021/01/DSC_0641_result-100x100.jpg" ,
+    category: "footwear" ,
+    size: "8" ,
+    color: "black" ,
+    clouse: "slipon" ,
+},
+{
+    image:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0903_result.jpg" ,
+    name: "WOODS MEN’S / CASUAL PARTY SHOES IN BLACK COLOUR",
+    price: "₹3,295.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0903_result-100x100.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0904_result-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0905_result-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0906_result-100x100.jpg" ,
+    category: "footwear" ,
+    size: "7" ,
+    color: "black" ,
+    clouse: "slipon" ,
+},
+{
+    image:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0911_result.jpg" ,
+    name: "WOODS MEN’S / CASUAL PARTY SHOES IN BLACK COLOUR",
+    price: "₹3,495.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0911_result-100x100.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0912_result-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0913_result-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0914_result-100x100.jpg" ,
+    category: "Men's footwear" ,
+    size: "6" ,
+    color: "black" ,
+    clouse: "slipon" ,
+},
+{
+    image:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0943_result.jpg" ,
+    name: "WOODS MEN’S / CASUAL PARTY SHOES IN BLACK COLOUR",
+    price: "₹2,995.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0943_result.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0944_result-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0945_result-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0946_result-100x100.jpg" ,
+    category: "Men's footwear" ,
+    size: "9" ,
+    color: "black" ,
+    clouse: "slipon" ,
+},
+{
+    image:"https://www.uboric.com/wp-content/uploads/2021/02/DSC_0951_result.jpg" ,
+    name: "WOODS MEN’S / CASUAL PARTY SHOES IN BLACK COLOUR",
+    price: "₹3,295.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0951_result-100x100.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0952_result-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0953_result-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2021/02/DSC_0954_result-100x100.jpg" ,
+    category: "party " ,
+    size: "8" ,
+    color: "black" ,
+    clouse: "slipon" ,
+},
+{
+    image:"https://www.uboric.com/wp-content/uploads/2019/12/16-11091.jpg" ,
+    name: "P&G MEN’S CASUAL/PARTY SHOES in BLUE/BROWN",
+    price: "₹1,399.00" ,
+    simage1: "https://www.uboric.com/wp-content/uploads/2019/12/16-11091-100x100.jpg" ,
+    simage2: "https://www.uboric.com/wp-content/uploads/2019/12/16-11092-100x100.jpg" ,
+    simage3: "https://www.uboric.com/wp-content/uploads/2019/12/16-11093-100x100.jpg" ,
+    simage4: "https://www.uboric.com/wp-content/uploads/2019/12/16-11094-100x100.jpg" ,
+    category: "party " ,
+    size: "8" ,
+    color: "brown" ,
+    clouse: "laceup" ,
+},
+                
+ ]
+
+ localStorage.setItem("list" , JSON.stringify(data)) ;
+ display(data)
+
+ function display(data){
+     data.map(function(el){
+         var box = document.createElement("div") ;
+         box.setAttribute("id" , "box") ;
+
+         var image = document.createElement("img") ;
+         image.setAttribute("id" , "img") ;
+         image.setAttribute("src" , el.image) ;
+
+         var name = document.createElement("p") ;
+         name.textContent = el.name ;
+
+         var price = document.createElement("h2") ;
+         price.textContent = el.price ;
+
+
+         box.append(image,name,price) ;
+         document.querySelector("#product").append(box) ;
+     })
+ }
